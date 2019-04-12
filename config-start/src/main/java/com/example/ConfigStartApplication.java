bb945cc839cbb21be8d3f13c0fbecce23b7a2f8c
123 <529075990@qq.com>
2019-04-12 20:43:09 +0800

    initial commit

---
+++ config-start/src/main/java/com/example/ConfigStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.configstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigStartApplication.class, args);
+    }
+
+}
